2bd02e8661d720569089e3514b048d648c140d39
derechtenap <50623315+derechtenap@users.noreply.github.com>
2020-01-13 13:07:31 +0100

    Aufgabe 14.1
    Voll funktionisfähig

---
+++ js/14_1_script.js
@@ -0,0 +1,9 @@
+// Skript für die Aufgabe 14.1
+
+function setzeFarbe(farbe) {
+    if("rot" === farbe) {
+        document.getElementById("farbe").style.color = "#ff0000";
+    } else if ("gruen" === farbe) {
+        document.getElementById("farbe").style.color = "#008000";
+    }
+}
