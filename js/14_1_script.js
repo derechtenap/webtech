16ce000df5bf75d27132532318a81e1563b4d32d
derechtenap <50623315+derechtenap@users.noreply.github.com>
2020-01-13 13:37:11 +0100

    14.1 Aufgabenteil b

--- js/14_1_script.js
+++ js/14_1_script.js
@@ -1,9 +1,23 @@
 // Skript für die Aufgabe 14.1
 
 function setzeFarbe(farbe) {
-    if("rot" === farbe) {
+    if ("rot" === farbe) {
         document.getElementById("farbe").style.color = "#ff0000";
     } else if ("gruen" === farbe) {
         document.getElementById("farbe").style.color = "#008000";
     }
+}
+
+// Aufgabenteil b
+
+function sternHinzu() {
+    var img = document.createElement("img");
+    img.src = "../grafiken/stern.png";
+    var src = document.getElementById("sterne");
+    src.appendChild(img);
+}
+
+function sternEntfernen() {
+  var stern = document.getElementById("sterne");
+  stern.removeChild(stern.childNodes[0]);
 }
