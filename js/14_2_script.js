d56c2b6884bfaa640ad8821e3e5cbec396097dd6
derechtenap <50623315+derechtenap@users.noreply.github.com>
2020-11-11 08:37:30 +0100

    14.2 verbessert :smile:

--- js/14_2_script.js
+++ js/14_2_script.js
@@ -1,32 +1,37 @@
 // JavaScript für 14.2 Aufgabe: 
 // Laden von JSON Elementen mit AJAX
 
-var stadt, ausgabe, anfrage = new XMLHttpRequest();
+var stadt, ausgabe, req = new XMLHttpRequest();
 
-function ladeStadt() {
-    stadt = event.target.value;
-    ladeDaten(stadt);
+function getCity() {
+    const city = event.target.value;
+    loadJSON(city);
 }
 
-function istHauptsadt(inpJSON) {
-    if (inpJSON.capital === true) {
-        return "the capital city of";
+// Überprüft ob eine Stadt eine Hauptstadt ist
+function isCapital(city) {
+    if (city.capital) {
+        return 'the capital city of';
     }
-    return "a city of";
+    return 'a city of';
 }
 
-function ladeDaten(stadt) {
-    anfrage.onreadystatechange = function() {
-        if (anfrage.readyState === 4) {
-            ausgabe = JSON.parse(anfrage.response);
-            console.log(anfrage.response);
-            document.getElementById("titel").innerHTML = ausgabe.name;
-            document.getElementById("inhalt").innerHTML = ausgabe.name + 
-            " is " + istHauptsadt(ausgabe) + " " + ausgabe.state + 
-            " with a metropolitan area of " + ausgabe.residents + ". " + 
-            ausgabe.remark;
+// Lädt die Daten per XMLHttpRequest
+function loadJSON(city) {
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) { // Anfrage hat funktioniert
+            data = JSON.parse(req.response);
+
+            const title = document.getElementById('titel');
+            const content = document.getElementById('inhalt');
+
+            // Die Daten auf der Seite anzeigen
+            title.innerHTML = data.name;
+            content.innerHTML = `${data.name} is ${isCapital(data)} ${data.state}` + 
+            `with a metropolitan area of ${data.residents}. ${data.remark}`;
         }
     }
-    anfrage.open("GET", "../json/" + stadt + ".json");
-    anfrage.send();
+
+    req.open("GET", "../json/" + city + ".json");
+    req.send();
 }
