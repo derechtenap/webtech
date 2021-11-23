932bcebd67ce643dedf1cf551d2fda340c4f1bc3
derechtenap <50623315+derechtenap@users.noreply.github.com>
2021-11-23 18:35:12 +0100

    5.1a

---
+++ aufgaben/blatt-5/5-1a-variablen-und-aufgaben.php
@@ -0,0 +1,13 @@
+<?php 
+    $x = 2;
+    $y = 5;
+
+    echo $x;            echo "<br/>";
+    echo "$x";          echo "<br/>";
+    echo '$x';          echo "<br/>";
+    echo $x + $y;       echo "<br/>";
+    echo "$x + $y";     echo "<br/>";
+    echo '$x + $y';     echo "<br/>";
+    echo "$x \"+\" $y"; echo "<br/>";
+    echo "Das Ergebnis von $x + $y ist " . ($x + $y);
+?>
